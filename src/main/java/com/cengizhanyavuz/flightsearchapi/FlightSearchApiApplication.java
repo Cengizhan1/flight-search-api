1a43930ff2240e805b79d98f5822a0ce10d7224b
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-01-25 17:43:33 +0300

    spring boot jpa and actuator added

--- src/main/java/com/cengizhanyavuz/flightsearchapi/FlightSearchApiApplication.java
+++ src/main/java/com/cengizhanyavuz/flightsearchapi/FlightSearchApiApplication.java
@@ -1,11 +1,24 @@
 package com.cengizhanyavuz.flightsearchapi;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
+@SpringBootApplication(exclude = {
+		//SecurityAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class FlightSearchApiApplication {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(FlightSearchApiApplication.class, args);
 	}

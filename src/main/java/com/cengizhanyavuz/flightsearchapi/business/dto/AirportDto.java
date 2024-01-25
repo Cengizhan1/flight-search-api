0e25806642f7ebde02f6d1ccd4b6cf503442030d
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-01-25 18:12:43 +0300

    dto added

---
+++ src/main/java/com/cengizhanyavuz/flightsearchapi/business/dto/AirportDto.java
@@ -0,0 +1,20 @@
+package com.cengizhanyavuz.flightsearchapi.business.dto;
+
+import com.cengizhanyavuz.flightsearchapi.auditing.AuditingAwareBaseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+public class AirportDto extends AuditingAwareBaseDto implements Serializable {
+    private Long id;
+    private String city;
+}
---
+++ src/main/java/com/cengizhanyavuz/flightsearchapi/business/dto/FlightDTO.java
@@ -0,0 +1,24 @@
+package com.cengizhanyavuz.flightsearchapi.business.dto;
+
+import com.cengizhanyavuz.flightsearchapi.auditing.AuditingAwareBaseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+public class FlightDTO extends AuditingAwareBaseDto implements Serializable {
+    private Long id;
+    private AirportDto departureAirport;
+    private AirportDto arrivalAirport;
+    private String departureDateTime;
+    private String returnDateTime;
+    private double price;
+}

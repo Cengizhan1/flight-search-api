0322ec6e83f72e60b39b6465d279058f01fa5908
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-01-27 16:16:33 +0300

    auth addded

---
+++ src/main/java/com/cengizhanyavuz/flightsearchapi/auth/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.cengizhanyavuz.flightsearchapi.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.management.relation.Role;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+  private String firstname;
+  private String lastname;
+  private String email;
+  private String password;
+  private Role role;
+}
